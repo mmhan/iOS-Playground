4cdfd93e066d905290273644d638498429a49115
Mike Han <mmhan2u@gmail.com>
2011-12-22 18:30:32 +0800

    Experimenting App Lifecycle

---
+++ Nav/NavTests/NavTests.h
@@ -0,0 +1,13 @@
+//
+//  NavTests.h
+//  NavTests
+//
+//  Created by Mike Han on 12/22/11.
+//  Copyright (c) 2011 Comwerks Interactive. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NavTests : SenTestCase
+
+@end
---
+++ Nav/NavTests/NavTests.m
@@ -0,0 +1,32 @@
+//
+//  NavTests.m
+//  NavTests
+//
+//  Created by Mike Han on 12/22/11.
+//  Copyright (c) 2011 Comwerks Interactive. All rights reserved.
+//
+
+#import "NavTests.h"
+
+@implementation NavTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NavTests");
+}
+
+@end
